1cbefd6a5a34b5adf19add4c87a0ad4d7196f39f
Sergey <stutunnikov74@gmail.com>
2023-03-21 20:20:24 +0700

    Added media files

--- products/models.py
+++ products/models.py
@@ -5,6 +5,8 @@ class Genre(models.Model):
     name=models.CharField(max_length=100)
     date=models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 
 class Game(models.Model):
     title=models.CharField(max_length=100)
@@ -14,4 +16,13 @@ class Game(models.Model):
     owner=models.ForeignKey(User,on_delete=models.CASCADE)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
+    description=models.TextField(blank=True,null=True)
+    image=models.ImageField(blank=True,null=True)
+    def __str__(self):
+        return f'{self.title} - {self.company}'
 # genre,owner,image,characters,характеристики(eng),mb more~~
+# git commit -m"Updated Model/Created Genre"
+
+# git add -A
+# git commit -m""
+# git push
