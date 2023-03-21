b2c85b3019ec00bc6d726fc7ecd83f1f267991ea
Sergey <stutunnikov74@gmail.com>
2023-03-21 20:55:53 +0700

    Added home page

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -11,6 +11,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'pages'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- e_shop/urls.py
+++ e_shop/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from pages.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_page)
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
