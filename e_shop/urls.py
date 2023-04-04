61232ca054dd0eaebf56f738ebc344390537a6c4
Sergey <stutunnikov74@gmail.com>
2023-04-04 20:51:39 +0700

    finished shop page

--- e_shop/urls.py
+++ e_shop/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from pages.views import home_page
+from pages.views import home_page,ShopPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home_page)
+    path('',home_page),
+    path('shop/',ShopPage.as_view(),name='shop')
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from products.models import Game
+from django.views.generic import ListView 
 # Create your views here.
 def home_page(request):
     return render(request,'home.html')
+
+class ShopPage(ListView):
+    template_name="shop.html"
+    model=Game
+    context_object_name="games"
